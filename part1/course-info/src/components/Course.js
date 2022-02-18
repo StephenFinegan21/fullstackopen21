e9368c59a845b2b69b6ecedd034fef796d88a072
StephenFinegan21 <x20191952@student.ncirl.com>
2022-02-18 23:09:16 +0000

    exercises 2.5

--- part1/course-info/src/components/Course.js
+++ part1/course-info/src/components/Course.js
@@ -4,19 +4,24 @@ import Content from './Content'
 import Header from './Header'
 
 const Course = ({ course }) => {
-    //console.log(course, 'is the props')
+    console.log(course, 'is the props')
    // console.log(course.name, 'is name of course')
+  
+  
   return (
     <div>
-    
-      <Header course={course.name} />
-        
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-        
-        
+    {
+        course.map(courses =>
+            <>
+                <Header course={courses.name} />
+                <Content parts={courses.parts} />
+                <Total parts={courses.parts} />
+             </>
+        ) 
+    }   
     </div>
   )
 }
+
 
 export default Course
